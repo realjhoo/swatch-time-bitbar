ae3f579d159bb97e01383121d3fa41a729b7efbd
Jerry L Hoover <jerry.hoover@ymail.com>
2021-02-20 09:48:32 -0600

    initial commit

---
+++ beats.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env /usr/local/bin/node
+// shebang for bitbar - comment out line 1 for web deploy
+
+/*
+Internet Time Bitbar Plugin
+J L Hoover
+Version 1.0 2021.02.20 (9 Sol 2271)
+*/
+
+// Bitbar Metadata
+// bitbar.title - Internet Time
+// bitbar.version - Version 1.0
+// bitbar.author - Jerry L Hoover
+// bitbar.author.github - realjhoo
+// bitbar.desc - Swatch internet time (.beats)
+// bitbar.image - working...
+// bitbar.dependencies - working...
+// bitbar.abouturl - URL to about
